1ea2102fa5d0294630206d8080073ec1f0ff62a2
artem.lab <artem.lab@localhost>
2011-01-20 20:13:57 +0500

    src tree

--- global/constants.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CONSTS_H
-#define _CONSTS_H
-
-enum result_type{RESULT_FAIL=0,RESULT_SUCCESS=1};
-
-#endif// _CONSTS_H
--- string/string.h
+++
@@ -1,7 +0,0 @@
-#ifndef _STRING_H_
-#define _STRING_H_
-
-#include "utf8string.h"
-typedef utf8string String;
-
-#endif //_STRING_H_
