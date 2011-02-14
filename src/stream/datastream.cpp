7f13789d115cca7bea6751290b0e428b95b32d75
artem.lab <artem.lab@localhost>
2011-02-14 11:36:54 +0500

    datastream

--- src/stream/datastream.cpp
+++ src/stream/datastream.cpp
@@ -11,3 +11,149 @@ DataStream::~DataStream()
 {
 }
 
+DataStream& DataStream::operator>>(int8_t& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(uint8_t& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(int16_t& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(uint16_t& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(int32_t& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(uint32_t& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(int64_t& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(uint64_t& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(float& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(double& data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(char* const data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+DataStream& DataStream::operator>>(unsigned char* const data)
+{
+    DataInputStream::operator>>(data);
+    return *this;
+}
+
+
+DataStream& DataStream::operator<<(int8_t& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(uint8_t& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(int16_t& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(uint16_t& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(int32_t& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(uint32_t& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(int64_t& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(uint64_t& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(float& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(double& data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(char* const data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+DataStream& DataStream::operator<<(unsigned char* const data)
+{
+    DataOutputStream::operator<<(data);
+    return *this;
+}
+
+
--- src/stream/datastream.h
+++ src/stream/datastream.h
@@ -12,6 +12,33 @@ class DataStream
 public:
     DataStream(IO::ReadableWritable* rw);
     ~DataStream();
+
+    DataStream& operator>>(int8_t& data);
+    DataStream& operator>>(uint8_t& data);
+    DataStream& operator>>(int16_t& data);
+    DataStream& operator>>(uint16_t& data);
+    DataStream& operator>>(int32_t& data);
+    DataStream& operator>>(uint32_t& data);
+    DataStream& operator>>(int64_t& data);
+    DataStream& operator>>(uint64_t& data);
+    DataStream& operator>>(float& data);
+    DataStream& operator>>(double& data);
+    DataStream& operator>>(char* const data);
+    DataStream& operator>>(unsigned char* const data);
+
+    DataStream& operator<<(int8_t& data);
+    DataStream& operator<<(uint8_t& data);
+    DataStream& operator<<(int16_t& data);
+    DataStream& operator<<(uint16_t& data);
+    DataStream& operator<<(int32_t& data);
+    DataStream& operator<<(uint32_t& data);
+    DataStream& operator<<(int64_t& data);
+    DataStream& operator<<(uint64_t& data);
+    DataStream& operator<<(float& data);
+    DataStream& operator<<(double& data);
+    DataStream& operator<<(char* const data);
+    DataStream& operator<<(unsigned char* const data);
+
 private:
     IO::ReadableWritable* _rw;
 };
