82df9824ffb9ef9d807cf078c87186940b4fc59b
artem.lab <artem.lab@localhost>
2011-02-15 23:46:03 +0500

    textstream

--- src/stream/textoutputstream.cpp
+++ src/stream/textoutputstream.cpp
@@ -1,6 +1,8 @@
 #include "textoutputstream.h"
 #include <stdio.h>
-
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#include <float.h>
 
 TextOutputStream::TextOutputStream(IO::Writable* w)
 {
@@ -14,78 +16,80 @@ TextOutputStream::~TextOutputStream()
 TextOutputStream& TextOutputStream::operator<<(const int8_t& data)
 {
     char text[5] = {0};//-100
-    sprintf(text, "%d", data);
-    _w->write(&text, sizeof(text));
+    int s = sprintf(text, "%d", data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const uint8_t& data)
 {
     char text[4] = {0};//255
-    sprintf(text, "%u", data);
-    _w->write(&text, sizeof(text));
+    int s = sprintf(text, "%u", data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const int16_t& data)
 {
     char text[7] = {0};//-10000
-    sprintf(text, "%d", data);
-    _w->write(&text, sizeof(text));
+    int s = sprintf(text, "%d", data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const uint16_t& data)
 {
     char text[6] = {0};//65535
-    sprintf(text, "%u", data);
-    _w->write(&text, sizeof(text));
+    int s = sprintf(text, "%u", data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const int32_t& data)
 {
     char text[12] = {0};//-2000000000
-    sprintf(text, "%d", data);
-    _w->write(&text, sizeof(text));
+    int s = sprintf(text, "%d", data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const uint32_t& data)
 {
     char text[11] = {0};//4294967295
-    sprintf(text, "%u", data);
-    _w->write(&text, sizeof(text));
+    int s = sprintf(text, "%u", data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const int64_t& data)
 {
-    char text[22] = {0};//-10000000000000000000
-    sprintf(text, "%lld", data);
-    _w->write(&text, sizeof(text));
+    char text[22] = {0};//-10000000000000000000 || -9000000000000000000 ?
+    int s = sprintf(text, "%" PRId64, data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const uint64_t& data)
 {
     char text[21] = {0};//18446744073709551615
-    sprintf(text, "%llu", data);
-    _w->write(&text, sizeof(text));
+    int s = sprintf(text, "%" PRIu64, data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const float& data)
 {
-    char text[4] = {0};
-    sprintf(text, "%f", data);
-    _w->write(&text, sizeof(text));
+    char text[FLT_MANT_DIG + 6] = {0}; // xxxxx.yyyy E-zz
+    int s = sprintf(text, "%f", data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
 TextOutputStream& TextOutputStream::operator<<(const double& data)
 {
-    _w->write(&text, sizeof(text));
+    char text[DBL_MANT_DIG + 6] = {0}; // xxxxx.yyyy E-zz
+    int s = sprintf(text, "%f", data);
+    if(s > 0) _w->write(text, s);
     return *this;
 }
 
