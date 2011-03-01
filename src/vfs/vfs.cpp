29412d16b2f054448524f341528364a83a184bc0
artem.lab <artem.lab@localhost>
2011-03-01 23:25:09 +0500

    vfs::file removed

--- src/vfs/vfs.cpp
+++ src/vfs/vfs.cpp
@@ -1,148 +1,6 @@
 #include "vfs.h"
-#include "glmath/mat2.h"
-//#include <cstdio>
-
-/*
- * File
- */
-
-VFS::File::File()
-{
-    _f = NULL;
-}
-
-VFS::File::~File()
-{
-    close();
-}
-
-bool VFS::File::open(const String& fn, IO::openmode_t om)
-{
-    char md[4] = {0};
-    int i = 0;
-
-    /*if(om & IO::File::OpenMode::Append){
-        md[i++]='a';
-        if(om & IO::File::OpenMode::Read){
-            md[i++]='+';
-        }
-    }else{
-        if(om & IO::File::OpenMode::Truncate ||
-           om & IO::File::OpenMode::Write){
-            md[i++]='w';
-            if(om & IO::File::OpenMode::Read){
-                md[i++]='+';
-            }
-        }else{
-            md[i++]='r';
-            if(om & IO::File::OpenMode::Write){
-                md[i++]='+';
-            }
-        }
-    }*/
-// rwa+
-    
-    if(om & IO::OpenMode::Truncate){
-        md[i++] = 'w';
-        if(om & IO::OpenMode::Read){
-            md[i++] = '+';
-        }
-    }else if(om & IO::OpenMode::Append){
-        md[i++] = 'a';
-        if(om & IO::OpenMode::Read){
-            md[i++] = '+';
-        }
-    }else if(om & IO::OpenMode::Write){
-        if(om & IO::OpenMode::Read){
-            md[i++] = 'r';
-            md[i++] = '+';
-        }else{
-            md[i++] = 'w';
-        }
-    }else{// if(om & IO::OpenMode::Read){
-        md[i++] = 'r';
-    }
-
-    if(om & IO::OpenMode::Binary){
-        md[i++]='b';
-    }
-    //fprintf(stdout,md);
-    _f = ::fopen(fn.c_str(), md);
-
-    return _f != NULL;
-}
-
-bool VFS::File::close()
-{
-    if(_f != NULL){
-        int res = ::fclose(_f);
-        if(res == 0){
-            _f = NULL;
-            return true;
-        }
-    }
-    return false;
-}
-
-IO::datasize_t VFS::File::size()
-{
-    IO::offset_t old_pos = tell();
-
-    if(seek(0, IO::SeekOrigin::End) == false){
-        return 0;
-    }
-    
-    IO::offset_t end_pos = tell();
-
-    seek(old_pos, IO::SeekOrigin::Begin);
-
-    return static_cast<IO::datasize_t>(end_pos);
-}
-
-bool VFS::File::seek(IO::offset_t p, IO::seekorigin_t o)
-{
-    int orgn = 0;
-    switch(o){
-        default:
-        case IO::SeekOrigin::Begin:
-            orgn = SEEK_SET;
-            break;
-        case IO::SeekOrigin::Current:
-            orgn = SEEK_CUR;
-            break;
-        case IO::SeekOrigin::End:
-            orgn = SEEK_END;
-            break;
-    }
-    return ::fseek(_f, p, orgn) == 0;
-}
-
-IO::offset_t VFS::File::tell()
-{
-    return ::ftell(_f);
-}
-
-IO::datasize_t VFS::File::read(void* data, IO::datasize_t s)
-{
-    if(::fread(data, s, 1, _f) == 1){
-        return s;
-    }
-    return 0;
-}
-
-IO::datasize_t VFS::File::write(const void* data, IO::datasize_t s)
-{
-    if(::fwrite(data, s, 1, _f) == 1){
-        return s;
-    }
-    return 0;
-}
-
-bool VFS::File::ungetc(char c)
-{
-    return ::ungetc(c, _f) == c;
-}
-
+#include <fstream>
+#include "fileinfilebuf.h"
 
 /*
  * VFS
@@ -154,22 +12,14 @@ VFS::VFS() {
 VFS::~VFS() {
 }
 
-IO::File* VFS::open(const String& fn, IO::openmode_t om)
+std::iostream* VFS::open(const String& fn, std::ios_base::openmode om)
 {
-    File* res = new File;
-    if(!res->open(fn, om)){
-        delete res;
-        return NULL;
-    }
+    std::fstream* res = new std::fstream(fn.c_str(), om);
     return res;
 }
 
-bool VFS::close(IO::File* f)
+bool close(std::iostream* iost)
 {
-    bool res = f->close();
-    if(res){
-        delete f;
-    }
-    return res;
+    delete iost;
+    return true;
 }
-
--- src/vfs/vfs.h
+++ src/vfs/vfs.h
@@ -1,43 +1,18 @@
 #ifndef _VFS_H
 #define	_VFS_H
 
-#include "io/io.h"
 #include "string/string.h"
-#include <cstdio>
+#include <iostream>
 
 
 class VFS {
 public:
 
-    class File
-        :public IO::File
-    {
-    public:
-        File();
-        ~File();
-
-        bool open(const String& fn, IO::openmode_t om);
-        bool close();
-
-        IO::datasize_t size();
-
-        bool seek(IO::offset_t p, IO::seekorigin_t o);
-        IO::offset_t tell();
-        
-        IO::datasize_t read(void* data, IO::datasize_t s);
-        IO::datasize_t write(const void* data, IO::datasize_t s);
-        
-        bool ungetc(char c);
-
-    private:
-        FILE* _f;
-    };
-
     VFS();
     ~VFS();
     
-    IO::File* open(const String& fn, IO::openmode_t om);
-    bool close(IO::File* f);
+    std::iostream* open(const String& fn, std::ios_base::openmode om);
+    bool close(std::iostream* iost);
 private:
 
 };
