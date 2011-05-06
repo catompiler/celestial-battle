07a8b87885541eae52ca26f64a5227a319b3d25d
artem.lab <artem.lab@localhost>
2011-05-06 20:21:00 +0600

    number_cast

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,13 @@
 #include <stdlib.h>
 #include <iostream>
-#include "vfs/vfs.h"
-#include "vfs/localfsentrypoint.h"
+#include "utils/utils.h"
 
 int main(int argc, char** argv)
 {
-    LocalFSEntryPoint cdEntry(".");
-
-    VFS vfs;
-
-    vfs.addEntryPoint(&cdEntry);
-
-    std::iostream* iost = vfs.open("test.txt", std::ios::out);
-
-    if(iost){
-        (*iost)<<"test";
-        vfs.close(iost);
-    }
-
+    const char* str = "12345";
+    
+    std::cout << utils::casts::number_cast<int>(str) << std::endl;
+    
     return (EXIT_SUCCESS);
 }
 
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -3,8 +3,24 @@
 
 namespace utils{
 
+
+/*
+ * casts
+ */
+namespace casts{
+
+//! cast string in number
+template<class T> T number_cast(const char*);
+
+}//casts
+
+
+/*
+ * functors
+ */
 namespace functors{
 
+//! delete single object
 struct delete_single{
     template<class T>
     void operator()(T* obj){
@@ -12,6 +28,7 @@ struct delete_single{
     }
 };
 
+//! delete array of objects
 struct delete_array{
     template<class T>
     void operator()(T* arr){
@@ -23,5 +40,7 @@ struct delete_array{
 }//functors
 
 }//utils
+
+#include "utils/utils_tmpl.h"
 
 #endif	//_UTILS_H_
