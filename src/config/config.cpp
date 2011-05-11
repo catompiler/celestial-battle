54c038f2a708980c23e796d264ef41bc511a45f3
artem.lab <artem.lab@localhost>
2011-05-11 14:48:28 +0600

    'config'

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -1430,7 +1430,11 @@ bool Config::read(std::istream& ist_)
     
     //std::cout << config_str;
     
-    return parse(config_begin, config_end) == config_end;
+    if(parse(config_begin, config_end) == config_end){
+        clear();
+        return false;
+    }
+    return true;
 }
 
 void Config::clear()
