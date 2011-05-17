d25db07f3b7cb94865450c50854789ce0190b28a
artem.lab <artem.lab@localhost>
2011-05-18 00:28:04 +0600

    x11 error handler

--- src/window/x11/x11_window.cpp
+++ src/window/x11/x11_window.cpp
@@ -453,17 +453,21 @@ void X11Window::_term_x11()
 int X11Window::_errorHandler(Display* display_, XErrorEvent* e)
 {
     #define MAX_ERROR_TEXT_LEN 31
-    #define MAX_ERROR_DB_TEXT_LEN 255
     char error_text_buf[MAX_ERROR_TEXT_LEN + 1];
-    char error_db_text_buf[MAX_ERROR_DB_TEXT_LEN + 1];
+    
+    /*#define MAX_ERROR_DB_TEXT_LEN 255
+    char error_db_text_buf[MAX_ERROR_DB_TEXT_LEN + 1];*/
     
     XGetErrorText(display_, e->error_code, error_text_buf, MAX_ERROR_TEXT_LEN);
-    error_text_buf[0] = e->request_code;
-    std::cout << "e->request_code == " << e->request_code << std::endl;
-    XGetErrorDatabaseText(display_, "XRequest", error_text_buf,
-            "Unknown error", error_db_text_buf, MAX_ERROR_DB_TEXT_LEN);
+    //std::cout << error_text_buf << std::endl;
     
-    log(Log::Error) << "X11 Error:" << std::endl << error_db_text_buf << std::endl;
+    /*XGetErrorDatabaseText(display_, "XlibMessage", "XRequest",
+            "Unknown error", error_db_text_buf, MAX_ERROR_DB_TEXT_LEN);*/
+    
+    log(Log::Error) << "X11 Error: " << error_text_buf << "; "
+        << "Major reques opcode: " << static_cast<unsigned int>(e->request_code)
+        << "; Minor reques opcode: " << static_cast<unsigned int>(e->minor_code)
+        << std::endl;
     
     return 0;
 }
