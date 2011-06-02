7be16045d8a570b274bef517f1e0905a9fbb0750
artem.lab <artem.lab@localhost>
2011-06-02 17:37:16 +0600

    #ifdef/elif

--- src/input/input.cpp
+++ src/input/input.cpp
@@ -3,12 +3,9 @@
 #include "osal/osdef.h"
 #ifdef OS_WINDOWS
 #include "win/win_input.h"
-#else
-#ifdef OS_LINUX
+#elif defined OS_LINUX
 #include "x11/x11_input.h"
-#else
-#endif  //OS_LINUX
-#endif  //OS_WINDOWS
+#endif
 
 namespace input{
 
@@ -19,13 +16,11 @@ state_t* state()
     return 
     #ifdef OS_WINDOWS
         win_state()
-    #else
-    #ifdef OS_LINUX
+    #elif defined OS_LINUX
         x11_state()
     #else
         NULL
-    #endif  //OS_LINUX
-    #endif  //OS_WINDOWS
+    #endif
     ;//return
 }
 
@@ -33,12 +28,9 @@ void setpos(int x, int y)
 {
     #ifdef OS_WINDOWS
         win_setpos(x, y);
-    #else
-    #ifdef OS_LINUX
+    #elif defined OS_LINUX
         x11_setpos(x, y);
-    #else
-    #endif  //OS_LINUX
-    #endif  //OS_WINDOWS
+    #endif
 }
 
 }//mouse
